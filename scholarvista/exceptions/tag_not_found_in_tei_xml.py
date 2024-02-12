80fdbfdc4b46ef9b6ed761706ce523c5407368fa
mciccale <marcociccalebaztan@gmail.com>
2024-02-12 11:33:00 +0100

    add custom exception for TEI XML parsing

---
+++ scholarvista/exceptions/tag_not_found_in_tei_xml.py
@@ -0,0 +1,12 @@
+"""
+This file contains the TagNotFound exception, which is thrown when a Tag is not found
+in a TEI XML file.
+"""
+
+class TagNotFoundInTeiXmlException(Exception):
+    def __init__(self, message: str | None = None) -> None:
+        """
+        Initializes the TagNotFoundInTeiXmlException object with an optional message.
+        """
+        self.message = message
+        super().__init__(message)
